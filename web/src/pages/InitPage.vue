fe85836c777cb87361ee69ef9187b6882766dbe0
小龙 <>
2026-05-06 18:20:57 +0800

    Revert "Step 4 远程 URL 模式加"复用现有本地目录"字段(umbrella 切出去的子模块场景)"
    This reverts commit ba37755f8af0bc5089a71d02c0e51955715ca690.

--- web/src/pages/InitPage.vue
+++ web/src/pages/InitPage.vue
@@ -2444,7 +2444,6 @@ const {
   refreshRoleHint,
   pickCloneTarget,
   pickLocalRepoDir,
-  pickRemoteLocalReuse,
   scanSingleRepo,
 } = useRepoScan({
   repoBranchesMap,
@@ -2904,7 +2903,6 @@ provide(WizardStoreKey, {
         @sub-path-input="(r) => onRepoSubPathInput(r)"
         @pick-clone-target="(r) => pickCloneTarget(r)"
         @pick-local-repo-dir="(r) => pickLocalRepoDir(r)"
-        @pick-remote-local-reuse="(r) => pickRemoteLocalReuse(r)"
         @scan-single-repo="(r) => scanSingleRepo(r)"
         @toggle-submodule-pick="(r, sub, checked) => toggleSubmodulePick(r, sub, checked)"
         @split-monorepo="(idx) => splitMonorepo(idx)"
